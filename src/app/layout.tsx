91e0227eaae267e808244e145c7958f5bc0ed9dd
karangosal <kgosal03@uvic.ca>
2025-08-15 11:02:36 -0700

    make changes to the nav menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css"
 import type { Metadata } from "next"
-import { ThemeProvider } from "../components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider"
 import { ModeToggle } from "@/components/mode-toggle"
 import {
   NavigationMenu,
@@ -24,29 +24,38 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className="min-h-screen flex flex-col bg-background text-foreground">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <NavigationMenu className="p-4 flex justify-between items-center">
-            <NavigationMenuList className="flex space-x-2">
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link href="/" className="px-4 py-2">Home</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link href="/blogs" className="px-4 py-2">Blogs</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link href="/projects" className="px-4 py-2">Projects</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            </NavigationMenuList>
+          
+          {/* NAVIGATION BAR */}
+          <nav className="w-full flex justify-center border-b border-border">
+            <div className="flex items-center justify-between w-[80%] max-w-6xl px-4 py-3">
+              
+              {/* Left: Links */}
+              <NavigationMenu>
+                <NavigationMenuList className="flex space-x-2">
+                  <NavigationMenuItem>
+                    <NavigationMenuLink asChild>
+                      <Link href="/" className="px-4 py-2">Home</Link>
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <NavigationMenuLink asChild>
+                      <Link href="/blogs" className="px-4 py-2">Blogs</Link>
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <NavigationMenuLink asChild>
+                      <Link href="/projects" className="px-4 py-2">Projects</Link>
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
 
-            {/* Dark Mode Toggle */}
-            <ModeToggle />
-          </NavigationMenu>
+              {/* Right: Dark Mode Toggle */}
+              <ModeToggle />
+            </div>
+          </nav>
 
+          {/* Page Content */}
           <main className="flex-1">{children}</main>
         </ThemeProvider>
       </body>
