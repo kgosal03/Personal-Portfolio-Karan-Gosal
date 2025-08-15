539220ec71303a5ea0f30106ad454a98eb831574
karangosal <kgosal03@uvic.ca>
2025-08-15 10:52:51 -0700

    add basic website

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,8 @@
+export default function Blogs() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">Blogs</h1>
+      <p className="text-gray-600">Here are some of my latest blog posts...</p>
+    </main>
+  )
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">Projects</h1>
+      <p className="text-gray-600">Here are some of my cool projects...</p>
+    </main>
+  )
+}
